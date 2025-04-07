105cd14ad34ea38148adf80d48881d8425b4f208
jaboikova <yanis.twink@gmail.com>
2025-04-07 22:08:43 +0300

    add todo

--- index.js
+++ index.js
@@ -1,15 +1,73 @@
-const todoBlocks = document.querySelectorAll("div.list");
-
-todoBlocks.forEach(el => {
-    const todoList = document.createElement("ul");
-
-    for (let i = 0;i<3;i++) {
-        let todo = document.createElement("li");
-        todo.innerHTML = "Todo " + i;
-        todoList.appendChild(todo);
-    }
-
-    el.appendChild(todoList);
-});
+const todos = [
+    { id: 1, text: "Buy veggies", done: true },
+    { id: 2, text: "Drink water", done: false },
+    { id: 3, text: "Watch a movie", done: false }
+];
+localStorage.setItem("todos", JSON.stringify(todos));
 
 // Add todo to a list
+document.getElementById("createTodo").addEventListener("click", function(el) {
+    el.preventDefault();
+
+    let newId = todos.length + 1;
+    let todoText = document.getElementById("myTextarea").value;
+
+    const todo = {id: newId, text: todoText, done: false};
+
+    console.log(todo);
+
+    todos.push(todo);
+    renderTodos();
+});
+
+// Show todos
+function renderTodos() {
+    const list = document.getElementById('todoList');
+    list.innerHTML = "";
+
+    todos.forEach(todo => {
+        const li = document.createElement("li");
+        const checkbox = document.createElement("input")
+        const btn = document.createElement("button");
+
+
+        checkbox.type = "checkbox";
+        checkbox.className = "checkbox"
+        btn.textContent = "Delete"
+        btn.className = "btnDelete"
+
+        const textNode = document.createTextNode(todo.text);
+
+        li.appendChild(checkbox);
+        li.appendChild(textNode)
+        li.appendChild(btn)
+
+        list.appendChild(li);
+    });
+
+    // Delete todo from a list
+    const btns = document.getElementsByClassName("btnDelete");
+
+    for (let btn of btns) {
+        btn.addEventListener("click", function(el) {
+            el.preventDefault();
+            console.log("remove")
+    })}
+
+    // Check if task is done
+    const checkboxes = document.getElementsByClassName('checkbox');
+
+    for (let checkbox of checkboxes) {
+        checkbox.addEventListener('change', () => {
+            if (checkbox.checked) {
+            console.log("Checked ✅");
+            // todos[i].done = true;
+            } else {
+            console.log("Unchecked ❌");
+            // todos[i].done = false;
+            }
+        });   
+    }
+} 
+
+window.onload = renderTodos;
